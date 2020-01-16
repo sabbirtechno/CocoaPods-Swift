3b36d6a17787edf2470546ddbf647a93890146b2
Sabbir Hossain <smac@labs-mac.local>
2020-01-16 16:37:42 +0600

    Added CocoaPod

--- Cocopod/ViewController.swift
+++ Cocopod/ViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtview: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btn(_ sender: Any) {
+        
+        
+        Alamofire.request("https://api.darksky.net/forecast/5c74c06e261c64b3a2f6e1abfb7fe85d/37.8267,-122.4233").responseJSON { response in
+            print(response.request as Any)  // original URL request
+            print(response.response as Any) // URL response
+            print(response.result.value as Any)   // result of response serialization
+            
+            self.txtview.text = response.description
+        }
+        
+        
+    }
 
-    
-    
+
 }
 
